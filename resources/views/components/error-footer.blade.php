34774ad3124ff17f2330eb80286e29888e6c99c9
oddishodiqul <shodiqul@radiansolusi.com>
2023-09-25 01:09:15 +0700

    implement fortify

--- resources/views/components/error-footer.blade.php
+++ resources/views/components/error-footer.blade.php
@@ -1,3 +1,3 @@
 <div class="simple-footer mt-5">
-    Copyright &copy; Stisla 2018
+    Copyright &copy; Shodiqul 2023
 </div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages/blank-page', ['type_menu' => '']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+    })->name('home');
 });
+
+Route::get('/', function () {
+    return view('pages.auth.login', ['type_menu' => '']);
+});
+
+// Route::get('/login', function () {
+//     return view('pages.auth.login', ['type_menu' => '']);
+// })->name('login');
+
+// Route::get('/register', function () {
+//     return view('pages.auth.register', ['type_menu' => '']);
+// })->name('register');
+
+// Route::get('/forgot-password', function () {
+//     return view('pages.auth.forgot-password', ['type_menu' => '']);
+// })->name('forgot-password');
+
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.reset-password', ['type_menu' => '']);
+// })->name('reset-password');
