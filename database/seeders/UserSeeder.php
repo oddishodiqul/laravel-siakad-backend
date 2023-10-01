bc4a9e77febb17134a5e7775389239793dae5a5a
oddishodiqul <shodiqul@radiansolusi.com>
2023-10-02 00:53:05 +0700

    crud user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,7 +21,7 @@ class UserSeeder extends Seeder
             'email' => 'oddi@example.com',
             'email_verified_at' => now(),
             'password' => Hash::make('12345678'),
-
+            'roles' => 'mahasiswa',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('home', function () {
         return view('pages.app.dashboard-siakad', ['type_menu' => '']);
     })->name('home');
+
+    Route::resource('user', UserController::class);
 });
 
 Route::get('/', function () {
